7fc1a7ce7a828f1be49e33dcf34a192057125c68
joshschusterman <josh.schusterman@gmail.com>
2021-02-27 21:33:53 -0600

    Updated calculator app
    Basic calculator. I manual created the interface, rather than using tables and buttons in HTML. It was a good way to practice CSS, and grid specifically. The only problem with this app right now is that the negative function doesn't work correctly when dividing or subtracting. The eval function doesn't recognize is next to a minus sign or divide sign. To be fixed soon. Need to customize this more as well.

--- calculator/scripts.js
+++ calculator/scripts.js
@@ -1,3 +1,24 @@
-function output() {
-    alert('test');  
+// Make a variable out of the output box html, so I can use methods on it.
+var out = document.querySelector('.out');
+
+function output(val) {
+    // Put number or operator pressed into a variable, so can use methods...
+    var press = val.innerHTML;
+
+    // Calculate (with eval) whatever is in the html for 'out' (the output box)
+    if (press == '=') {
+        out.innerHTML = eval(out.innerHTML);
+    } else if (press == "C") {
+        // Zero out if C is pressed.
+        out.innerHTML = '0';
+    } else {
+        // If the output box is at 0, display only the key just pressed.
+        if (out.innerHTML == '0') {
+            out.innerHTML = press;
+        } else {
+            // If there are already numbers or operators, include those (extend the equation).
+            out.innerHTML += press;
+        }
+    }
 }
+
